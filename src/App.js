4ce042cc07e2ac8b155c0e912d1545c95efe9657
msjeon <jmlambda@naver.com>
2020-07-20 16:54:52 +0900

    After Setting

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default () => (
+    <div>
+      <p>only this paragraph will get the style :)</p>
+  
+      { /* you can include <Component />s here that include
+           other <p>s that don't get unexpected styles! */ }
+  
+      <style jsx>{`
+        p {
+          color: red;
+        }
+      `}</style>
+    </div>
+  )
